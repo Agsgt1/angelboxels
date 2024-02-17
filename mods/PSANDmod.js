8c2962d7ad6cb98e5a1e8182099243c385a43c3b
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-02-17 15:00:12 -0600

    Create PSANDmod.js

---
+++ mods/PSANDmod.js
@@ -0,0 +1,29 @@
+/*
+
+Food Food Food! Adds more food to Sandboxels.
+Created by sugima.
+
+Sandboxels Plus v1
+
+Changelog - 1/25/24 - v1
+ - Made the mod :|
+ -Added pink sand and pink particles
+ 
+*/
+elements.pparticle = {
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state: "solid",
+    color: ["#FF11FF","#FFFF11"],
+    reactions: {},
+};
+
+elements.psand = {
+    behavior: behaviors.POWDER,
+    category: "land",
+    state: "solid",
+    color: ["#FF00FF","#00FF00"],
+    breakInto: "pparticle",
+    breakIntoColor: "#FF01FF",
+    reactions: {},
+};
